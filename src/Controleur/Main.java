a1454abcf6f2d2512b7b6be653a214b1a7a9c503
paulhfisher <fisherhpaul@gmail.com>
2020-12-14 15:32:24 +0100

    commit : PopUp pour nb de joueurs

--- src/Controleur/Main.java
+++ src/Controleur/Main.java
@@ -1,4 +1,5 @@
 
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -23,12 +24,8 @@ public class Main {
         
         Interface test = new Interface();
         Musique son = new Musique();
-        son.start(); // lancement du thread
+        //son.start(); // lancement du thread
         test.Container();
         test.setVisible(true);
-        test.jeu();
- 
-
     }
-
 }
