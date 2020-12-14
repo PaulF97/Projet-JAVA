778e3d441241d1688bc6159ffb26a5ea6ffdee71
paulhfisher <fisherhpaul@gmail.com>
2020-12-14 15:32:02 +0100

    commit : PopUp pour nb de joueurs

--- src/Controleur/J_Ordinateur.java
+++ src/Controleur/J_Ordinateur.java
@@ -11,9 +11,6 @@ package Controleur;
  */
 public class J_Ordinateur extends Joueur{
     
-    @Override
-    public void tourJeu() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
+    
     
 }
